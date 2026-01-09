e6a8279d33a49b9bd1ed2550cb905870676f6c45
AdrianGonzalezBlanco <ikdvp@plaiaundi.net>
2026-01-09 14:08:58 +0100

    segundo commit no esta ni login hecho

--- Mobile-App/MAVPC/app/src/main/java/com/example/mavpc/Login.java
+++ Mobile-App/MAVPC/app/src/main/java/com/example/mavpc/Login.java
@@ -1,7 +1,13 @@
 package com.example.mavpc;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
 }
